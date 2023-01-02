ed87c358f9a3c53342bd457ff4555652969d8edb
KenueY <kenueyy@yandex.ru>
2023-01-02 17:17:22 +0700

    add namespaces

--- Assets/Scripts/Entities/BaseHealthController.cs
+++ Assets/Scripts/Entities/BaseHealthController.cs
@@ -8,69 +8,73 @@ using System;
 using UnityEngine;
 using Utilities.Behaviours;
 
-[Register(typeof(IHealthView))]
-internal class BaseHealthController : KernelEntityBehaviour, IHealthView
+namespace Entities
 {
-    public event Action onHealthChanged;
-    public float MaxHealth
+
+    [Register(typeof(IHealthView))]
+    internal class BaseHealthController : KernelEntityBehaviour, IHealthView
     {
-        get => _maxHealth;
-        set
+        public event Action onHealthChanged;
+        public float MaxHealth
         {
-            _maxHealth = value;
+            get => _maxHealth;
+            set
+            {
+                _maxHealth = value;
+            }
         }
-    }
-    public float CurrentHealth
-    {
-        get => _currentHealth;
-        set
+        public float CurrentHealth
         {
-            _currentHealth = Mathf.Clamp(value, 0, MaxHealth);
-            onHealthChanged?.Invoke();
+            get => _currentHealth;
+            set
+            {
+                _currentHealth = Mathf.Clamp(value, 0, MaxHealth);
+                onHealthChanged?.Invoke();
+            }
         }
+
+        private float _maxHealth;
+        private float _currentHealth;
+
+        [ContextMenu("Damage")]
+        public void TakeDamage()
+        {
+            CurrentHealth -= 5;
+        }
+
+        private void TakeDamage(IDamageDealer damageDealer)
+        {
+            CurrentHealth -= damageDealer.Damage;
+        }
+
+        private void Initialize(IEntityData entity)
+        {
+            MaxHealth = entity.Data.MaxHealth;
+            CurrentHealth = entity.Data.MaxHealth;
+        }
+
+        #region KernelEntity
+
+        private IEntityData _entityData;
+        private IDamagable _damageController;
+
+
+        [ConstructMethod]
+        private void Construct(IKernel kernel)
+        {
+            _damageController = kernel.GetInjection<IDamagable>();
+            _entityData = kernel.GetInjection<IEntityData>();
+
+            Initialize(_entityData);
+
+            _damageController.onColliderEnter += TakeDamage;
+        }
+
+        protected override void OnDispose()
+        {
+            _damageController.onColliderEnter -= TakeDamage;
+        }
+
+        #endregion
     }
-
-    private float _maxHealth;
-    private float _currentHealth;
-
-    [ContextMenu("Damage")]
-    public void TakeDamage()
-    {
-        CurrentHealth -= 5;
-    }
-
-    private void TakeDamage(IDamageDealer damageDealer)
-    {
-        CurrentHealth -= damageDealer.Damage;
-    }
-
-    private void Initialize(IEntityData entity)
-    {
-        MaxHealth = entity.Data.MaxHealth;
-        CurrentHealth = entity.Data.MaxHealth;
-    }
-
-    #region KernelEntity
-
-    private IEntityData _entityData;
-    private IDamagable _damageController; 
-
-
-    [ConstructMethod]
-    private void Construct(IKernel kernel)
-    {
-        _damageController = kernel.GetInjection<IDamagable>();
-        _entityData = kernel.GetInjection<IEntityData>();
-
-        Initialize(_entityData);
-
-        _damageController.onColliderEnter += TakeDamage;
-    }
-
-    protected override void OnDispose()
-    {
-        _damageController.onColliderEnter -= TakeDamage;
-    }
-
-    #endregion
 }
--- Assets/Scripts/Entities/Hero/Cleric.cs
+++ Assets/Scripts/Entities/Hero/Cleric.cs
@@ -1,30 +1,29 @@
 using DI.Attributes.Construct;
 using DI.Interfaces.KernelInterfaces;
-using Entities.Hero;
 using Entities.Interfaces;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-internal class Cleric : Hero
+namespace Entities.Hero
 {
-    public float Health;
-    public float CurrentHealth;
-
-    [ConstructField]
-    private IHealthView healthView;
-
-
-    private void Change()
+    internal class Cleric : Hero
     {
-        CurrentHealth = healthView.CurrentHealth;
-    }
+        public float Health;
+        public float CurrentHealth;
 
-    [ConstructMethod]
-    private void Construct(IKernel kernel)
-    {
-        healthView.onHealthChanged += Change;
-        Health = healthView.MaxHealth;
-    }
+        [ConstructField]
+        private IHealthView healthView;
 
+
+        private void Change()
+        {
+            CurrentHealth = healthView.CurrentHealth;
+        }
+
+        [ConstructMethod]
+        private void Construct(IKernel kernel)
+        {
+            healthView.onHealthChanged += Change;
+            Health = healthView.MaxHealth;
+        }
+
+    }
 }
