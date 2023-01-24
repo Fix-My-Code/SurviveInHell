e24f5a2c8eddc7260f4eda31cd5cc8cf02075aa6
KenueY <kenueyy@yandex.ru>
2023-01-24 23:22:13 +0700

    Выставил валидные значения для OrderLayer

--- Assets/Scripts/Utilities/SpriteSorter.cs
+++ Assets/Scripts/Utilities/SpriteSorter.cs
@@ -6,7 +6,7 @@ namespace Utilities
     public class SpriteSorter : MonoBehaviour
     {
         [SerializeField]
-        [Range(-1.0f, 1.0f)]
+        [Range(-10.0f, 10.0f)]
         private float offset;
 
         private int _sortingOrderBase = 0;      
