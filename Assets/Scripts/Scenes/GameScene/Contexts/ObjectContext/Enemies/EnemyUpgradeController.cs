45acecfed2a0acff6eeaf38affedce10582b507c
KenueY <kenueyy@yandex.ru>
2023-02-15 21:12:56 +0700

    добавил неймспейс

--- Assets/Scripts/Scenes/GameScene/Contexts/ObjectContext/Enemies/EnemyUpgradeController.cs
+++ Assets/Scripts/Scenes/GameScene/Contexts/ObjectContext/Enemies/EnemyUpgradeController.cs
@@ -6,54 +6,57 @@ using UnityEngine;
 using Utilities;
 using Utilities.Behaviours;
 
-internal class EnemyUpgradeController : KernelEntityBehaviour
+namespace ObjectContext.Enemies
 {
+    internal class EnemyUpgradeController : KernelEntityBehaviour
+    {
 
-    public void OnTimeUpgradeHandler()
-    {
-        RandomUpgrade();
-    }
-    private void OnEnable()
-    {
-        if (IsInitialize)
+        public void OnTimeUpgradeHandler()
         {
             RandomUpgrade();
         }
-    }
-
-    private void RandomUpgrade()
-    {
-        var randomInt = Randomizer.RandomIntValue(0,3);
-
-        switch (randomInt)
+        private void OnEnable()
         {
-            case 0:
-                _damageBuff.Increase(_enemyData.Data.DamageUpgradePercent);
-                break;
-
-            case 1:
-                _healthBuff.IncreaseHealth(_enemyData.Data.HealthUpgradePercent);
-                break;
-
-            case 2:
-                _speedBuff.Increase(_enemyData.Data.SpeedUpgradePercent);
-                break;
+            if (IsInitialize)
+            {
+                RandomUpgrade();
+            }
         }
-    }
 
-    private IDamageBuff _damageBuff;
-    private IHealthBuff _healthBuff;
-    private ISpeedBuff _speedBuff;
+        private void RandomUpgrade()
+        {
+            var randomInt = Randomizer.RandomIntValue(0, 3);
 
-    private IEnemyData _enemyData;
+            switch (randomInt)
+            {
+                case 0:
+                    _damageBuff.Increase(_enemyData.Data.DamageUpgradePercent);
+                    break;
 
-    [ConstructMethod]
-    private void Construct(IKernel kernel)
-    {
-        _damageBuff = kernel.GetInjection<IDamageBuff>();
-        _healthBuff = kernel.GetInjection<IHealthBuff>();
-        _speedBuff = kernel.GetInjection<ISpeedBuff>();
-        _enemyData = kernel.GetInjection<IEnemyData>();
-        IsInitialize = true;
+                case 1:
+                    _healthBuff.IncreaseHealth(_enemyData.Data.HealthUpgradePercent);
+                    break;
+
+                case 2:
+                    _speedBuff.Increase(_enemyData.Data.SpeedUpgradePercent);
+                    break;
+            }
+        }
+
+        private IDamageBuff _damageBuff;
+        private IHealthBuff _healthBuff;
+        private ISpeedBuff _speedBuff;
+
+        private IEnemyData _enemyData;
+
+        [ConstructMethod]
+        private void Construct(IKernel kernel)
+        {
+            _damageBuff = kernel.GetInjection<IDamageBuff>();
+            _healthBuff = kernel.GetInjection<IHealthBuff>();
+            _speedBuff = kernel.GetInjection<ISpeedBuff>();
+            _enemyData = kernel.GetInjection<IEnemyData>();
+            IsInitialize = true;
+        }
     }
 }
