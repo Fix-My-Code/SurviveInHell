9058f736b687a869ecf1efdc2ef6e0412adf4090
KenueY <kenueyy@yandex.ru>
2023-02-22 00:08:31 +0700

    Сделал карту тройного бафа

---
+++ Assets/Scripts/Scenes/GameScene/Contexts/UIContext/PlayerUI/SpellCards/TripleBuffCard.cs
@@ -0,0 +1,23 @@
+using DI.Attributes.Construct;
+using DI.Interfaces.KernelInterfaces;
+using DI.Kernels;
+using UIContext.ChooseBuffPanel;
+using UIContext.PlayerUI.SkillCards;
+
+internal class TripleBuffCard : SkillCard
+{
+    private async protected override void Action()
+    {
+        base.Action();
+        await _upgradePanel.ShowUpgradePanel();
+        await _upgradePanel.ShowUpgradePanel();
+        await _upgradePanel.ShowUpgradePanel();
+    }
+
+    private IUpgradePanel _upgradePanel;
+    [ConstructMethod(typeof(UiSceneKernel))]
+    private void Construct(IKernel kernel)
+    {
+        _upgradePanel = kernel.GetInjection<IUpgradePanel>();
+    }
+}
