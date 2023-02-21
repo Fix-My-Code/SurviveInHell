e39438ea5e4105a03c071255d6e889d9c88a8ae9
KenueY <kenueyy@yandex.ru>
2023-02-22 00:14:20 +0700

    загрузил неймспейсы

--- Assets/Scripts/Scenes/GameScene/Contexts/UIContext/PlayerUI/SpellCards/TripleBuffCard.cs
+++ Assets/Scripts/Scenes/GameScene/Contexts/UIContext/PlayerUI/SpellCards/TripleBuffCard.cs
@@ -2,22 +2,24 @@ using DI.Attributes.Construct;
 using DI.Interfaces.KernelInterfaces;
 using DI.Kernels;
 using UIContext.ChooseBuffPanel;
-using UIContext.PlayerUI.SkillCards;
 
-internal class TripleBuffCard : SkillCard
+namespace UIContext.PlayerUI.SkillCards
 {
-    private async protected override void Action()
+    internal class TripleBuffCard : SkillCard
     {
-        base.Action();
-        await _upgradePanel.ShowUpgradePanel();
-        await _upgradePanel.ShowUpgradePanel();
-        await _upgradePanel.ShowUpgradePanel();
-    }
+        private async protected override void Action()
+        {
+            base.Action();
+            await _upgradePanel.ShowUpgradePanel();
+            await _upgradePanel.ShowUpgradePanel();
+            await _upgradePanel.ShowUpgradePanel();
+        }
 
-    private IUpgradePanel _upgradePanel;
-    [ConstructMethod(typeof(UiSceneKernel))]
-    private void Construct(IKernel kernel)
-    {
-        _upgradePanel = kernel.GetInjection<IUpgradePanel>();
+        private IUpgradePanel _upgradePanel;
+        [ConstructMethod(typeof(UiSceneKernel))]
+        private void Construct(IKernel kernel)
+        {
+            _upgradePanel = kernel.GetInjection<IUpgradePanel>();
+        }
     }
 }
